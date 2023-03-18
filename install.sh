675356da22ee87d9120aa60277eb878ea8cdf3cb
Cnbbx <admin@cnbbx.com>
2023-03-18 13:48:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-docker rm -f cnbbx_alist
-docker pull ghcr.io/cnbbx/cnbbx_alist:main
+LANG=zh_CN.UTF-8
+echo "Initialize directory"
 mkdir -p /root/alist
+docker rm -f cnbbx_alist
+echo "Update docker"
+docker pull ghcr.io/cnbbx/cnbbx_alist:main
+echo "Output password file: /root/alist/password.txt"
 docker run -it -v /root/alist:/opt/alist/data ghcr.io/cnbbx/cnbbx_alist:main /opt/alist/alist admin | tee /root/alist/password.txt
-cat /root/alist/password.txt
+echo "Modify CDN configuration"
 sed -i 's/"cdn":[^,]*/"cdn":"https:\/\/npm.elemecdn.com\/alist-web@$version\/dist\/"/' /root/alist/config.json
-docker run -d --name cnbbx_alist \
--v /root/alist:/opt/alist/data \
---security-opt seccomp:unconfined \
---restart=always \
--p 5244:5244 \
--d ghcr.io/cnbbx/cnbbx_alist:main
+echo "Start docker"
+docker run -d --name cnbbx_alist -v /root/alist:/opt/alist/data --security-opt seccomp:unconfined \
+--restart=always -p 5244:5244 -d ghcr.io/cnbbx/cnbbx_alist:main
