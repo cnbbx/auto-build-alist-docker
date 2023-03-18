f8e9c43a4c8a6f091bc102cbc281f3db59273b37
Cnbbx <admin@cnbbx.com>
2023-03-18 13:39:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 docker rm -f cnbbx_alist
 docker pull ghcr.io/cnbbx/cnbbx_alist:main
 mkdir -p /root/alist
-docker run -it -v /root/alist:/opt/alist/data ghcr.io/cnbbx/cnbbx_alist:main /opt/alist/alist admin > /root/alist/password.txt
+docker run -it -v /root/alist:/opt/alist/data ghcr.io/cnbbx/cnbbx_alist:main /opt/alist/alist admin | tee /root/alist/password.txt
 cat /root/alist/password.txt
 sed -i 's/"cdn":[^,]*/"cdn":"https:\/\/npm.elemecdn.com\/alist-web@$version\/dist\/"/' /root/alist/config.json
 docker run -d --name cnbbx_alist \
