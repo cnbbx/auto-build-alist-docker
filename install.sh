35bceae5ccac34c90bb04431cb1b5e67ec5569fa
Cnbbx <admin@cnbbx.com>
2024-02-20 11:35:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,8 @@ docker rm -f cnbbx_alist
 echo -e "\033[33m====== 更新Docker ======\033[0m"
 docker pull ghcr.io/cnbbx/cnbbx_alist:main
 echo -e "\033[33m====== 修改CDN配置 ======\033[0m"
-sed -i 's/"cdn":[^,]*/"cdn":"https:\/\/cdn.jsdelivr.net\/npm\/alist-web@$version\/dist\/"/' /root/alist/config.json
+sed -i 's/"cdn":[^,]*/"cdn":"https:\/\/npm.elemecdn.com\/alist-web@$version\/dist\/"/' /root/alist/config.json
 echo -e "\033[33m====== 启动Docker ======\033[0m"
 docker run -d --name cnbbx_alist -v /root/alist:/opt/alist/data --security-opt seccomp:unconfined \
 --restart=always -p 5244:5244 -d ghcr.io/cnbbx/cnbbx_alist:main
-echo -e "查询密码命令：\033[36mdocker exec -it cnbbx_alist ./alist admin\033[0m" 
-echo -e "\033[36m====== 显示密码 ======\033[0m"
-docker exec -i cnbbx_alist ./alist admin
+echo -e "\033[36m====== 启动成功 ======\033[0m"
